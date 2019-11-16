701006fdae7ecbb5f17c87939fca89e43aaad35c
Junior Alves <jjalves96@gmail.com>
2019-11-16 12:16:16 -0300

    Cria os controllers de User e Product

---
+++ src/controllers/UserController.js
@@ -0,0 +1,18 @@
+import User from '../models/User';
+
+module.exports = {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        email,
+        password,
+      });
+    }
+
+    return res.json(user);
+  },
+};
