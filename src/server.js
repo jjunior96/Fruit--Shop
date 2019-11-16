e748e66b8779a5be15df24c87a91738d27b195fa
Junior Alves <jjalves96@gmail.com>
2019-11-16 12:15:30 -0300

    Adiciona conexão com o MongoDB

--- src/server.js
+++ src/server.js
@@ -1,3 +1,19 @@
-import app from './app';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import routes from './routes';
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://semana:semana@cluster0-0laro.mongodb.net/fruit-shop?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000);
