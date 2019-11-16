9168029b544a85684b277c7c87add85b9c11e474
Junior Alves <jjalves96@gmail.com>
2019-11-16 12:14:41 -0300

    Adiciona rota de User e Product

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,11 @@
 import Router from 'express';
 
+import UserController from './controllers/UserController';
+import ProductController from './controllers/ProductController';
+
 const routes = new Router();
 
-const users = [{ name: 'Junior', email: 'junior@email.com' }];
-
-function checkUserExists(req, res, next) {
-  const { email } = req.body;
-  const userExists = users.find(user => user.email === email);
-
-  if (userExists) {
-    return res.status(400).json({ error: 'Usuario ja existe' });
-  }
-
-  return next();
-}
-
-routes.get('/users', (req, res) => {
-  return res.json(users);
-});
-
-routes.post('/users', checkUserExists, (req, res) => {
-  const { name, email } = req.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  users.push(user);
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/products', ProductController.store);
 
 export default routes;
