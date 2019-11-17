b9562714f600e34d341b1ff00041fbd057adc06d
Junior Alves <jjalves96@gmail.com>
2019-11-17 09:23:47 -0300

    Uploads de imagem com o Multer

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import Router from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 
 import UserController from './controllers/UserController';
 import ProductController from './controllers/ProductController';
 
 const routes = new Router();
+const upload = multer(uploadConfig);
 
 routes.post('/users', UserController.store);
-routes.post('/products', ProductController.store);
+routes.post('/products', upload.single('image'), ProductController.store);
 
 export default routes;
