13e0107e144d2cebabdebec95fcaa39049a8919e
Junior Alves <jjalves96@gmail.com>
2019-11-18 09:23:20 -0300

    Adiciona informacoes do usuario no carrinho de compras

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -4,13 +4,18 @@ module.exports = {
   async store(req, res) {
     const { user_id } = req.headers;
     const { product_id } = req.params;
-    // const { total } = req.body;
+    const { total } = req.body;
 
     const cart = await Cart.create({
       user: user_id,
       product: product_id,
-      // total,
+      total,
     });
+
+    await cart
+      .populate('product')
+      .populate('user')
+      .execPopulate();
 
     return res.json(cart);
   },
