b70cbb5498724906affbe19690ce841281c9191a
Junior Alves <jjalves96@gmail.com>
2019-11-18 08:26:25 -0300

    Verifica se usuario existe no banco

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,10 +1,17 @@
 import Product from '../models/Product';
+import User from '../models/User';
 
 module.exports = {
   async store(req, res) {
     const { filename } = req.file;
     const { name, price, amount } = req.body;
     const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exists' });
+    }
 
     const product = await Product.create({
       user: user_id,
