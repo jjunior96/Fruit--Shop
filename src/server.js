7b8f92b5dbaa3771b17ed4ae590af695d14e06d7
Junior Alves <jjalves96@gmail.com>
2019-11-22 08:46:37 -0300

    Cria rota files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 
 import routes from './routes';
 
@@ -16,6 +17,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3000);
