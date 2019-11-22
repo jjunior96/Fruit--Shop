f33a148d9b7b88a4d62b73e051e7e10b24653168
Junior Alves <jjalves96@gmail.com>
2019-11-22 08:43:47 -0300

    Disponibiliza a url das imagens dos produtos

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,15 +1,26 @@
 import mongoose from 'mongoose';
 
-const ProductSchema = new mongoose.Schema({
-  image: String,
-  name: String,
-  price: Number,
-  amount: Number,
-  category: String,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+const ProductSchema = new mongoose.Schema(
+  {
+    image: String,
+    name: String,
+    price: Number,
+    amount: Number,
+    category: String,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+ProductSchema.virtual('image_url').get(function() {
+  return `http://localhost:3000/files/${this.image}`;
 });
 
 export default mongoose.model('Product', ProductSchema);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -13,6 +14,7 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
